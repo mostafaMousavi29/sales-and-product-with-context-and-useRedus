686b5d028ff692752c7bac70814baee6e7b280b7
mostafa mousavi <mostafa.mousavi73@gmail.com>
2023-04-07 16:54:45 +0430

    sales and product with context and useRedus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+
+// Components
+import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
+
+// Context
+import ProductContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductContextProvider>
+      <CartContextProvider>
+        <Navbar />
+        <Switch>
+          <Route path="/products/:id" component={ProductDetails} />
+          <Route path="/products" component={Store} />
+          <Route path="/cart" component={ShopCart} />
+          <Redirect to="/products" />
+        </Switch>
+      </CartContextProvider>
+    </ProductContextProvider>
   );
 }
 
